a7dda4fd10218ad1e090af43612a165f4c5bfcd0
frank <13732629211@163.com>
2017-06-22 14:31:25 +0800

    用户的登录功能

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+
+
+use Illuminate\Http\Request;
+
+
+class SessionController extends Controller
+{
+    /**
+     * 用户登录页面展示
+     */
+    public function create(){
+
+        return view('sessions.create');
+    }
+
+    /**
+     * 创建会话信息(用户登录)
+     */
+    public function store(Request $request){
+        $this->validate($request,[
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+
+        if(Auth::attempt($credentials,$request->has('remember'))){
+            session()->flash('success','欢迎回来!');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger','很抱歉，您的邮箱和密码不匹配!');
+        }
+    }
+
+    /**
+     * 删除会话信息(用户退出)
+     */
+    public function destory(){
+        Auth::logout();
+        session()->flash('success','您已成功退出!');
+        return redirect('login');
+    }
+}
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frank
+ * Date: 2017/6/22
+ * Time: 上午9:58
+ */
